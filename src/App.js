431e744ea40ccd369364a436461056b2e1a0985c
lgavlich <lgavlich@gmail.com>
2022-05-29 00:53:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import AppBar from "./components/AppBar/AppBar";
-import Container from "./components/Container/Container";
-import HomeView from "./views/HomeView";
-import PokemonView from "./views/PokemonView";
+//import Container from "./components/Container/Container";
+import HomeView from "./views/HomeView/HomeView";
+import PokemonView from "./views/PokemonView/PokemonView";
 
 export default function App() {
   return (
-    <Container>
+    <>
       <AppBar />
       <Routes>
         <Route path="/" exact element={<HomeView />} />
 
         <Route path="/pokemon/:name" element={<PokemonView />} />
       </Routes>
-    </Container>
+    </>
   );
 }
